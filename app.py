16b388edada53c3a05a7e52ff40701bb59643d96
ashvp <ashwin.vp.2005@gmail.com>
2025-05-09 18:22:25 +0530

    Added CORS

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import joblib
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 model = joblib.load('phishing_predictor.pkl')
 vectorizer = joblib.load('tfidf_vectorizer.pkl')
